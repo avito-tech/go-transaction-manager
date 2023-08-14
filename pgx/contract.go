682ef05e41c92bcc86d10d0462ffa2ec65678258
hound672 <hound672@gmail.com>
2023-08-14 19:01:27 +0300

    Extend methods in tr interface

--- pgx/contract.go
+++ pgx/contract.go
@@ -11,11 +11,14 @@ import (
 // Tr is an interface to work with pgx.DB or pgx.Tx.
 // StmtContext and Stmt are not implemented!
 type Tr interface {
-	//Prepare(ctx context.Context, name, sql string) (*pgconn.StatementDescription, error)
+	Begin(ctx context.Context) (pgx.Tx, error)
+	BeginFunc(ctx context.Context, f func(pgx.Tx) error) (err error)
+
+	CopyFrom(ctx context.Context, tableName pgx.Identifier, columnNames []string, rowSrc pgx.CopyFromSource) (int64, error)
+	SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults
 
 	Exec(ctx context.Context, sql string, arguments ...interface{}) (commandTag pgconn.CommandTag, err error)
-
 	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
-
 	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+	QueryFunc(ctx context.Context, sql string, args []interface{}, scans []interface{}, f func(pgx.QueryFuncRow) error) (pgconn.CommandTag, error)
 }
