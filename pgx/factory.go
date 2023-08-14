9458a411050e3539f915be85418344419c5213ca
hound672 <hound672@gmail.com>
2023-08-14 19:38:57 +0300

    Use savepoint from sql package

--- pgx/factory.go
+++ pgx/factory.go
@@ -2,17 +2,18 @@ package pgx
 
 import (
 	"context"
+	trmsql "github.com/avito-tech/go-transaction-manager/sql"
 
 	"github.com/avito-tech/go-transaction-manager/trm"
 )
 
 // NewDefaultFactory creates default trm.Transaction(pgx.Tx).
 func NewDefaultFactory(db Transactional) trm.TrFactory {
-	return NewFactory(db, NewSavePoint())
+	return NewFactory(db, trmsql.NewSavePoint())
 }
 
 // NewFactory creates trm.Transaction(pgx.Tx).
-func NewFactory(db Transactional, sp SavePoint) trm.TrFactory {
+func NewFactory(db Transactional, sp trmsql.SavePoint) trm.TrFactory {
 	return func(ctx context.Context, trms trm.Settings) (context.Context, trm.Transaction, error) {
 		s, _ := trms.(Settings)
 
