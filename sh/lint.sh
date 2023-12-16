f20517de60a6ec3c6abe7efa43b2afadcbede847
iasergunin <iasergunin@avito.ru>
2023-12-16 22:42:58 +0400

    1. fix linters

--- sh/lint.sh
+++ sh/lint.sh
@@ -8,13 +8,13 @@ cd ../
 ROOT=$(pwd)
 
 echo "\ntrm"
-cd trm && golangci-lint run -c $ROOT/.golangci.yml --timeout=2m $@
+cd trm && golangci-lint run -c $ROOT/.golangci.yml --timeout=2m --path-prefix=trm $@
 cd $ROOT
 
 for driver in $drivers; do
   if [ -d "$driver" ]; then
     echo "\n$driver"
-    cd $driver && golangci-lint run -c $ROOT/.golangci.yml --timeout=2m $@
+    cd $driver && golangci-lint run -c $ROOT/.golangci.yml --timeout=2m --path-prefix=$driver $@
 
     cd $ROOT
   fi
