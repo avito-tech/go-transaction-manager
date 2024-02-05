043cf2ed7551345920d70cc8ab4c989e012279b7
Ilia Sergunin <ilia.sergunin@gmail.com>
2024-02-05 08:56:15 +0400

    1. Added rewrite description for gorm DisableNestedTransaction

--- gorm/factory.go
+++ gorm/factory.go
@@ -11,7 +11,8 @@ import (
 	"github.com/avito-tech/go-transaction-manager/trm"
 )
 
-// NewDefaultFactory creates default trm.Transaction(sqlx.Tx).
+// NewDefaultFactory creates default trm.Transaction(gorm.DB).
+// Factory rewrites DisableNestedTransaction in gorm.Config with Propagation in trm.Settings.
 func NewDefaultFactory(db *gorm.DB) trm.TrFactory {
 	return func(ctx context.Context, trms trm.Settings) (context.Context, trm.Transaction, error) {
 		s, _ := trms.(Settings)
